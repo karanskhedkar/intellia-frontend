3aa3202ab15c836fd07318fab2469b437305a24f
karan9505 <karanskhedkar@gmail.com>
2025-09-27 22:15:45 +0530

    cors updated

--- src/pages/Customize.jsx
+++ src/pages/Customize.jsx
@@ -7,6 +7,7 @@ import image4 from "../assets/image4.png";
 import image5 from "../assets/image5.png";
 import image6 from "../assets/image6.jpeg";
 import image7 from "../assets/image7.jpeg";
+import assistantImage from "../assets/assistantImage.png"
 import { RiImageAddLine } from "react-icons/ri";
 import { userDataContext } from "../context/UserContext";
 import { useNavigate } from "react-router-dom";
@@ -41,13 +42,13 @@ function Customize() {
         Select your <span className="text-blue-200">Assistant Image</span>
       </h1>
       <div className="w-full max-w-[900px] flex justify-center items-center flex-wrap gap-[15px]">
-        <Card image={image1} />
-        <Card image={image2} />
+        <Card image={assistantImage} />
+        {/* <Card image={image2} />
         <Card image={image3} />
         <Card image={image4} />
         <Card image={image5} />
         <Card image={image6} />
-        <Card image={image7} />
+        <Card image={image7} /> */}
         <div
           className={`w-[70px] h-[140px] lg:w-[150px] lg:h-[250px] bg-[#020220] border-2 border-[#0000ff66] rounded-2xl overflow-hidden hover:shadow-2xl hover:shadow-blue-950 cursor-pointer hover:border-4 hover:border-white flex items-center justify-center ${
             selectedImage == "input"
